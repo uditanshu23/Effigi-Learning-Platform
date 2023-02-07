d114e25818b7b2bd7d187f086fa9aede89275358
Uditanshu Satpathy <uditanshusatpathy23@gmail.com>
2023-02-07 15:25:51 +0530

    Basic Spring Functionality

---
+++ src/main/java/com/LearningPortal/spring/LearningPortal/LearningPortalApplication.java
@@ -0,0 +1,13 @@
+package com.LearningPortal.spring.LearningPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LearningPortal/spring/LearningPortal/LearningPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearningPortal.spring.LearningPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
