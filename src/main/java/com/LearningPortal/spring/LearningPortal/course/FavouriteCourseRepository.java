f6cc9894e32d1d81fedf69b6200b7e5d40ba9ce3
Uditanshu Satpathy <uditanshusatpathy23@gmail.com>
2023-02-10 02:31:31 +0530

    feat: repository

---
+++ src/main/java/com/LearningPortal/spring/LearningPortal/course/FavouriteCourseRepository.java
@@ -0,0 +1,7 @@
+package com.LearningPortal.spring.LearningPortal.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavouriteCourseRepository extends JpaRepository<FavouriteCourse, Long> {
+
+}
---
+++ src/main/java/com/LearningPortal/spring/LearningPortal/course/SubscribedCourseRepository.java
@@ -0,0 +1,7 @@
+package com.LearningPortal.spring.LearningPortal.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribedCourseRepository extends JpaRepository<SubscribedCourse, Long> {
+
+}
