35d3484eeb81629b2f50cfed135bc5aa0a8f40cb
Uditanshu Satpathy <uditanshusatpathy23@gmail.com>
2023-02-09 20:29:48 +0530

    fix: fixed import type error

--- src/main/java/com/LearningPortal/spring/LearningPortal/LearningPortalApplication.java
+++ src/main/java/com/LearningPortal/spring/LearningPortal/LearningPortalApplication.java
@@ -2,10 +2,8 @@ package com.LearningPortal.spring.LearningPortal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.LearningPortal.spring")
 public class LearningPortalApplication {
 
 	public static void main(String[] args) {
